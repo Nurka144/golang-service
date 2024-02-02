e765a1e67ed1c9b27e25e25970f28e6bb15c2e02
Nurka <tuzelnurbek@gmail.com>
2024-02-02 07:08:43 +0600

    hhtp get

--- internal/services/user.go
+++ internal/services/user.go
@@ -1,6 +1,10 @@
 package services
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/Nurka144/golang-service/internal/models"
 	"github.com/Nurka144/golang-service/internal/repository"
 )
@@ -16,6 +20,23 @@ func NewUserService(r repository.User) *UserService {
 }
 
 func (s *UserService) FindOne(id int) (*models.User, error) {
+	url := "https://jsonplaceholder.typicode.com/users/1"
+	resp, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, er := ioutil.ReadAll(resp.Body)
+
+	if er != nil {
+		return nil, er
+	}
+
+	fmt.Println("Ответ сервера", string(body.id))
+
 	return s.UserRepository.FindOne(id)
 }
 
